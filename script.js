eccb56c9fa506b961eeef4a84d87a60b22dbb0a6
Jarencio <145330400+Jarencio@users.noreply.github.com>
2024-10-15 20:58:49 +0800

    Final _ Touchments

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-document.addEventListener("contextmenu", (event) => event.preventDefault());
-// var larawan = ["https://banner2.cleanpng.com/20180619/uox/kisspng-my-singing-monsters-clip-art-singing-child-5b28eec8ad5b41.8535842215294092247101.jpg",
-//                 "https://img2.pngio.com/blue-monster-png-hd-cute-monster-clipart-free-transparent-png-cute-cartoon-monsters-png-840_512.png",
-//                 "https://thumbs.dreamstime.com/b/modern-cute-monster-character-cartoon-mascot-clipart-vector-illustration-modern-cute-monster-character-cartoon-mascot-clipart-169885914.jpg",
-//                 "https://www.clipartkey.com/mpngs/m/235-2355857_baby-child-monster-cartoon-cute-girl-people-.png",
-//                 "https://www.pinclipart.com/picdir/middle/53-531569_cookie-monster-cute-cookies-image-by-jazygirl-stop.png",
-//                 "https://st4.depositphotos.com/38459756/39410/v/950/depositphotos_394104742-stock-illustration-cartoon-cute-blue-monster-suitable.jpg"];
 
-// var larawan = [
-//   "https://drive.google.com/file/d/1vJFLx7eQKbBLyidPX4BMnMFGEDJvpVDH/view?usp=sharing",
-//   "Mike2.webp",
-//   "Sully_Monsters_University-removebg-preview.png",
-// ];
 
 var larawan = [
   "Character_Images/Bea NEath.png",
@@ -19,6 +7,11 @@ var larawan = [
   "Character_Images/Kitty.png",
   "Character_Images/Maggot.png",
   "Character_Images/Orchid.png",
+  "Character_Images/Orchid_Twin.png",
+  "Character_Images/Mildread.png",
+  "Character_Images/Frozen Charlotte.png",
+  "Character_Images/Camilla.png",
+  "Character_Images/Ava.png",
   "Character_Images/Toxic_Molty.png"
 ];
 
@@ -26,12 +19,27 @@ var i = 0;
 function add() {
   const div = document.createElement("div");
   div.className = "monster-profile";
-  div.innerHTML = `<img src="${
-    larawan[i]
-  }" class="img-fluid" alt="monster-image" style="width: 110px; height: 150px;"><h6 style="margin-left: 10px;">${
-    document.getElementById("monster-name").value
-  }</h6>`;
+  
+  const container = document.createElement("div");
+  container.className = "monster-container-inner";
+  
+  const img = document.createElement("img");
+  img.src = larawan[i];
+  img.className = "img-fluid";
+  img.alt = "monster-image";
+
+  
+  const name = document.createElement("h6");
+  name.style.marginLeft = "10px";
+  name.textContent = document.getElementById("monster-name").value;
+  
+  container.appendChild(img);
+  container.appendChild(name);
+  
+  div.appendChild(container);
+  
   document.getElementById("monster-container").appendChild(div);
+  
   i == larawan.length - 1 ? (i = 0) : i++;
 }
 
@@ -40,7 +48,13 @@ function pick() {
     const names = document.getElementById("monster-container").children;
     const selectedIndex = Math.floor(Math.random() * names.length);
     let intervalRuns = 0;
-  
+    
+    if (selectedIndex > larawan.length){
+      pic = selectedIndex - 7
+    } else {
+      pic = selectedIndex
+    }
+
     const interval1 = setInterval(() => {
       console.log(intervalRuns);
       
@@ -50,12 +64,13 @@ function pick() {
         const winnerDiv = document.createElement("div");
         winnerDiv.id = "ew";
         
-        // Create an image for the selected monster
-        const winnerMonsterImage = `<img src="${larawan[selectedIndex]}" alt="..." class="img-thumbnail" style="width: 300px; height: 300px;">`;
+        const winnerMonsterImage = `<img src="${larawan[pic]}" alt="..." class="img-thumbnail" ">`;
+        const winnerMonsterImages = `<img src="${larawan[pic]}" alt="..." class="img-thumbnail" ">`;
+
         winnerDiv.innerHTML = winnerMonsterImage;
+
         document.getElementById("winner").appendChild(winnerDiv);
   
-        // Set the winner name
         document.getElementById("winner-name").value = names[selectedIndex].textContent;
         
         clearInterval(interval1);
@@ -65,6 +80,6 @@ function pick() {
       }
       
       intervalRuns++;
-    }, 100);
+    }, 50);
   }
   
